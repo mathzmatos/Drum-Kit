2047765a6d4d17cf80cb02b2e81f7f2e708d3496
mathzmatos <cyrekt@hotmail.com>
2023-02-10 22:27:18 -0300

    keydown update

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+ 
+ //Detecting Button Press
+ 
+ 
  var numberOfDrumButtons = document.querySelectorAll(".drum").length;
  
  for (var i = 0; i< numberOfDrumButtons; i++)
@@ -6,13 +10,32 @@
            
             var buttonInnerHTML = this.innerHTML;
 
-            switch (buttonInnerHTML) 
-            {
+            makeSound(buttonInnerHTML);
+
+
+
+        });
+ }
+
+
+ //Detecting Keyboard Press
+
+ document.addEventListener("keydown", function(event)
+ {
+
+              makeSound(event.key);
+
+ });
+
+        
+ function makeSound(key)
+ {
+       switch(key)
+       {
               case "w":
                      var tom1 = new Audio("sounds/tom-1.mp3");
                      tom1.play();
                      break;
-
 
               case "a":
                      var tom2 = new Audio("sounds/tom-2.mp3");
@@ -38,28 +61,12 @@
                      var crash = new Audio("sounds/crash.mp3");
                      crash.play();
                      break;
-
+              
               case "l":
                      var kick = new Audio("sounds/kick-bass.mp3");
                      kick.play();
                      break;
-
-              default:
-                     console.log(buttonInnerHTML);
-            }
-
-
-
-
-
-
-
-
-
-        });
+              
+              default: console.log(buttonInnerHTML);
+       }
  }
-
-
-
-        //    var audio = new Audio("sounds/tom-1.mp3");
-        //    audio.play();
